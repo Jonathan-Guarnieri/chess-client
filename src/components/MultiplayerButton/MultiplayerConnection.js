7202ad95671480c042882e22f5f08be0a9837690
Jonathan Guarnieri <djou.guarnieri@hotmail.com>
2025-06-10 20:13:32 -0300

    remove unnecessary alerts

--- src/components/MultiplayerButton/MultiplayerConnection.js
+++ src/components/MultiplayerButton/MultiplayerConnection.js
@@ -4,14 +4,11 @@ import { useRouter } from 'next/navigation';
 import useApiCable from '@/hooks/useApiCable';
 
 export default function MultiplayerConnection() {
-  alert('descobrir pq isso aqui ta carregando 2x')
-
   const router = useRouter();
 
   useApiCable('MatchmakerChannel', callbackHandler);
 
   function callbackHandler(data) {
-    alert('passou aqui? pq deu match found no back....') // CONTINUAR DAQUI
     if (data.action === 'match_found') {
       router.push('/game');
     }
